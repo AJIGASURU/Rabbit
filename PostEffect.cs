589513e80154d1825a2fec946e416cc7916d14dd
Wakasugi <wakasugi.mamoru@x.colopl.co.jp>
2020-07-05 14:12:58 +0900

    ポストエフェクト

---
+++ PostEffect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]//Play中じゃなくても実行
+public class PostEffect : MonoBehaviour
+{
+
+    [SerializeField]
+    private Material _material;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture dest)
+    {
+        Graphics.Blit(source, dest, _material);
+    }
+}
